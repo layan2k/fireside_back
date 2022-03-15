2482a9b1f4140c7fd19929cbb9d0050d0298b19e
mrbridge-mrbridge <nanamcroj@gmail.com>
2022-03-15 01:02:25 +0000

    serializers

--- fireside_api/serializer.py
+++ fireside_api/serializer.py
@@ -8,12 +8,24 @@ class MovieSerializer(serializers.ModelSerializer):
     """Serializes  Movie data from complex data to json and vice versa. """
     class Meta:
         model = Movie
-        fields = "__all__"
+        fields = (
+            'uuid',
+            'title',
+            'description',
+            'date_created',
+            'movie_type',
+            'genre',
+            'age_limit',
+            'get_flyer',
+            'get_thumbnail',
+            'get_movies',
+            'videos',
+        )
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     """Serializes Profile data from complex data to json and vice versa. """
     class Meta:
         model = Profile
-        fields = "__all__"
+        fields = "__all__"
 
