7aaed516354c38b2c592ff3eab5bf958945766b0
layan2k <lesliedeeshumba@gmail.com>
2022-03-20 09:58:04 +0200

    Revert "Merge branch 'richard'"
    This reverts commit 32ba4a7b6367da6ba23e8f31e0c2299529399b8d, reversing
    changes made to 16870d03a60d43067d6166471b1ce0cb53b5ff9d.

--- fireside_api/serializer.py
+++ fireside_api/serializer.py
@@ -1,26 +1,14 @@
 """Serializers for data conversion"""
 from rest_framework import serializers
 
-from fireside_api.models import *
+from fireside_api.models import Movie, Profile
 
 
 class MovieSerializer(serializers.ModelSerializer):
     """Serializes  Movie data from complex data to json and vice versa. """
     class Meta:
         model = Movie
-        fields = (
-            'uuid',
-            'title',
-            'description',
-            'date_created',
-            'movie_type',
-            'genre',
-            'age_limit',
-            'get_flyer',
-            'get_thumbnail',
-            'get_movies',
-            'videos',
-        )
+        fields = "__all__"
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -28,4 +16,3 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = "__all__"
-
