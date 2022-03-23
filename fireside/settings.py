e517e72ce66a656474fe9beaf8d72016164db817
layan2k <lesliedeeshumba@gmail.com>
2022-03-23 11:24:17 +0200

    update

--- fireside/settings.py
+++ fireside/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL = '/static/'#location where django collect all static files
 STATIC_ROOT = os.path.join(BASE_DIR,'static')# location where you will store your static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'project_name/static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'fireside/static')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
